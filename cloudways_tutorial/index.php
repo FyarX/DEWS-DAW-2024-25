a0d03006f5fb818d675629119d46d5c0ca9e19ac
Aarón Ávila <150016544+FyarX@users.noreply.github.com>
2025-02-09 03:40:08 +0100

    Add files via upload

---
+++ cloudways_tutorial/index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorial de Hosting con Cloudways</title>
+</head>
+<body>
+    <h1>Esto es una página web hosteada con Cloudways</h1>
+    <p>Hecho por Aarón Ávila Moral para la asignatura de Desarrollo Web en Entorno Servidor</p>
+
+    <?php
+        echo "Esto es una prueba";
+    ?>
+</body>
+</html>
